25c7c0bd136f777b7c028df9749ec96f54433075
Simon Revill <34922287+simonrevill@users.noreply.github.com>
2023-09-22 15:26:19 +0100

    adds Subscribe section

--- src/sections/Subscribe.jsx
+++ src/sections/Subscribe.jsx
@@ -1,5 +1,23 @@
+import Button from "../components/Button";
+
 const Subscribe = () => {
-  return <div>Subscribe</div>;
+  return (
+    <section
+      id="contact-us"
+      className="max-container flex items-center justify-between gap-10 max-lg:flex-col"
+    >
+      <h3 className="font-palanquin text-4xl font-bold leading-[68px] lg:max-w-md">
+        Sign Up for
+        <span className="text-coral-red"> Updates </span>& Newsletter
+      </h3>
+      <div className="sm:border-slate-gray flex w-full items-center gap-5 rounded-full p-2.5 max-sm:flex-col sm:border lg:max-w-[40%]">
+        <input type="text" placeholder="subscribe@nike.com" className="input" />
+        <div className="flex items-center max-sm:w-full max-sm:justify-end">
+          <Button label="Sign Up" fullWidth />
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Subscribe;
