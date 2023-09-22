fa5e54eec33c656355d319261158ff0e431744c2
Simon Revill <34922287+simonrevill@users.noreply.github.com>
2023-09-22 14:58:52 +0100

    adds SpecialOffer section / updates Button props

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,12 +1,27 @@
-const Button = ({ label, iconURL }) => {
+const Button = ({
+  label,
+  iconURL,
+  backgroundColor,
+  textColor,
+  borderColor,
+  fullWidth,
+}) => {
   return (
-    <button className="font-montserrat leading-0 bg-coral-red flex items-center justify-center gap-2 rounded-full border px-7 py-4 text-lg text-white">
+    <button
+      className={`font-montserrat flex items-center justify-center gap-2 border px-7 py-4 text-lg leading-none
+      ${
+        backgroundColor
+          ? `${backgroundColor} ${textColor} ${borderColor}`
+          : "bg-coral-red border-coral-red text-white"
+      } rounded-full ${fullWidth && "w-full"}`}
+    >
       {label}
+
       {iconURL && (
         <img
           src={iconURL}
           alt="arrow right icon"
-          className="ml-2 h-5 w-5 rounded-full"
+          className="ml-2 h-5 w-5 rounded-full bg-white"
         />
       )}
     </button>
