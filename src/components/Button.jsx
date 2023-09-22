efa228f178b0d0793829518a40728b7e260b83f7
Simon Revill <34922287+simonrevill@users.noreply.github.com>
2023-09-22 09:00:37 +0100

    adds conditional icon for Button

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -2,11 +2,13 @@ const Button = ({ label, iconURL }) => {
   return (
     <button className="font-montserrat leading-0 bg-coral-red flex items-center justify-center gap-2 rounded-full border px-7 py-4 text-lg text-white">
       {label}
-      <img
-        src={iconURL}
-        alt="arrow right icon"
-        className="ml-2 h-5 w-5 rounded-full"
-      />
+      {iconURL && (
+        <img
+          src={iconURL}
+          alt="arrow right icon"
+          className="ml-2 h-5 w-5 rounded-full"
+        />
+      )}
     </button>
   );
 };
